a179381f14c9121998f95685469d2be4e4437f8f
Chris Wong <chriswong1224@gmail.com>
2014-08-04 23:58:59 -0400

    basic layout of roster page

--- scripts/urultimate.js
+++ scripts/urultimate.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
 
 	$(".menu").click(clickMenu);
 	$(window).on("resize", hideMenu);
+	$(window).on("resize", adjustHeight);
 });
 
 function clickMenu(){
@@ -15,9 +16,17 @@ function clickMenu(){
 }
 
 function hideMenu(){
-	var windowSize = $(window).width();
+	var window_size = $(window).width();
 
-	if(windowSize >= 767){
+	if(window_size >= 767){
 		$(".mobile-links").css("display","none");
 	}
+}
+
+function adjustHeight(){
+	//alert("began thingy");
+	//alert(cover_photo.height());
+	var adjusted_window_height = $(window).height - 103;
+	var cover_height = adjusted_window_height/2;
+	//$("#roster_cover").css('height', cover_height);
 }
